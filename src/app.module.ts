44a0ed8e1d838caa7d5651a5b76f61ad251973f5
asjha2 <asjha2@cisco.com>
2021-07-17 20:08:26 +0530

    (feat) : implements functional logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
+import { MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersController } from './customers/customers.controller';
 import { CustomersService } from './customers/customers.service';
+import { logger } from './logger.middleware';
 
 @Module({
   imports: [],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { logger } from 'src/logger.middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logger);
   await app.listen(3000);
 }
 bootstrap();
