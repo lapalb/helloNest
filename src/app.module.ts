174bcad7e1bc5905ed541438b4742be1fbce592a
asjha2 <asjha2@cisco.com>
2021-07-18 09:00:10 +0530

    feat : add user entity
    Signed-off-by: asjha2 <asjha2@cisco.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { CustomersController } from './customers/customers.controller';
 import { CustomersService } from './customers/customers.service';
 import { logger } from './logger.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController, CustomersController],
   providers: [AppService, CustomersService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,9 @@
+export class User {
+    public id: number;
+    public name: string;
+    public email: string;
+    public password: string;
+    public role: string;
+    public createdAt: Date;
+    public updatedAt: Date; 
+}
