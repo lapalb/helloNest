37f834380f72d4587cb57d6b5bf15bb08a04e2c4
asjha2 <asjha2@cisco.com>
2021-07-18 12:08:42 +0530

    (feat) : add health checks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersController } from './customers/customers.controller';
 import { CustomersService } from './customers/customers.service';
 import { logger } from './logger.middleware';
 import { UsersModule } from './users/users.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, CustomersController],
+  imports: [UsersModule, TerminusModule],
+  controllers: [AppController, CustomersController, HealthController],
   providers: [AppService, CustomersService],
 })
 export class AppModule {}
