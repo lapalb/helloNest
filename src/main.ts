21e7310e37662e1125c1545130e2627b49541363
asjha2 <asjha2@cisco.com>
2021-07-18 09:06:17 +0530

    (feat) : add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { logger } from 'src/logger.middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger);
+  const config = new DocumentBuilder()
+  .setTitle('Ashish Jha\'s Pet Project!')
+  .setDescription('The Ashish Jha API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
